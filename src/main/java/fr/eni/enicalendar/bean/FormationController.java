4af26b81e3a1c70f86e84d9b2ab569a2fad2acd6
piron <piron@10.128.200.20>
2018-05-15 11:01:03 +0100

    ajout formation

---
+++ src/main/java/fr/eni/enicalendar/bean/FormationController.java
@@ -0,0 +1,29 @@
+package fr.eni.enicalendar.bean;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+
+@ManagedBean(name="formationController")
+@ViewScoped
+public class FormationController implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+	@PostConstruct
+    public void setup()  {
+    	//formations = eniCalendarRepositoryService.findAllTitre();       
+    }
+	
+	
+	
+	
+}
