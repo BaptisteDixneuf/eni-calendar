37580796f5eebf3c1bd41ddc3d271a5c4603d1c9
Dixneuf Baptiste <baptiste.dixneuf@gmail.com>
2018-05-15 13:05:41 +0200

    cours example

--- src/main/java/fr/eni/enicalendar/service/impl/TitreService.java
+++ src/main/java/fr/eni/enicalendar/service/impl/TitreService.java
@@ -4,25 +4,23 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.eni.enicalendar.persistence.entities.Titre;
 import fr.eni.enicalendar.persistence.repositories.TitreRepository;
-import fr.eni.enicalendar.service.EniCalendarRepositoryService;
+import fr.eni.enicalendar.service.TitreServiceInterface;
 
 @Service
-public class EniCalendarRepositoryServiceImpl implements EniCalendarRepositoryService {
-	
-	 private static final Logger LOGGER = LoggerFactory.getLogger(EniCalendarRepositoryServiceImpl.class);
+public class TitreService implements TitreServiceInterface {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TitreService.class);
 
-	    @Autowired
-	    private TitreRepository titreRepository;
-	    
-	    public List<Titre> findAllTitre(){
-	    	LOGGER.info("findAllTitre");
-			return titreRepository.findAll();    	
-	    }
+	@Autowired
+	private TitreRepository titreRepository;
+
+	public List<Titre> findAllTitre() {
+		LOGGER.info("findAllTitre");
+		return titreRepository.findAll();
+	}
 }
