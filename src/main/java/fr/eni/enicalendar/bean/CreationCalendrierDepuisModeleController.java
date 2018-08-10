6a388e1d7f7f077d3de2edf4196093aee0405533
axel <pironaxel@gmail.com>
2018-08-10 16:42:15 +0200

    factorisation

--- src/main/java/fr/eni/enicalendar/bean/CreationCalendrierDepuisModeleController.java
+++ src/main/java/fr/eni/enicalendar/bean/CreationCalendrierDepuisModeleController.java
@@ -4,38 +4,41 @@ import fr.eni.enicalendar.persistence.app.entities.Utilisateur;
 import fr.eni.enicalendar.persistence.erp.entities.Lieu;
 import fr.eni.enicalendar.persistence.erp.entities.Stagiaire;
 import fr.eni.enicalendar.service.LieuServiceInterface;
-import fr.eni.enicalendar.service.UtilisateurServiceInterface;
 import fr.eni.enicalendar.service.StagiaireServiceInterface;
+import fr.eni.enicalendar.service.UtilisateurServiceInterface;
 import fr.eni.enicalendar.utils.SessionUtils;
 import org.primefaces.PrimeFaces;
 import org.primefaces.event.SelectEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-@ManagedBean(name = "creationCalendrierVideController")
+@ManagedBean(name = "creationCalendrierDepuisModeleController")
 @ViewScoped
-public class CreationCalendrierVideController implements Serializable {
+public class CreationCalendrierDepuisModeleController implements Serializable {
 
 	/**
 	 * Serial UID
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(CreationCalendrierVideController.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(CreationCalendrierDepuisModeleController.class);
+
+	@ManagedProperty(value = "#{utilisateurService}")
+	private UtilisateurServiceInterface utilisateurService;
 
 	@ManagedProperty(value = "#{stagiaireService}")
 	private StagiaireServiceInterface stagiaireService;
@@ -43,6 +46,7 @@ public class CreationCalendrierVideController implements Serializable {
 	@ManagedProperty(value = "#{lieuService}")
 	private LieuServiceInterface lieuService;
 
+	private Utilisateur utilisateur;
 	private Stagiaire stagiaire;
 	private Date date1;
 	private Date date2;
@@ -56,6 +60,10 @@ public class CreationCalendrierVideController implements Serializable {
 
 	public void setOption(String option) {
 		this.option = option;
+	}
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
 	}
 
 	public Stagiaire getStagiaire() {
@@ -94,13 +102,17 @@ public class CreationCalendrierVideController implements Serializable {
 		this.lieuxLibelles = lieuxLibelles;
 	}
 
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+
 	public void setStagiaire(Stagiaire stagiaire) {
 		this.stagiaire = stagiaire;
 	}
 
 	@PostConstruct
 	public void setup() {
-		LOGGER.info("CreationCalendrierVideController setup");
+		LOGGER.info("CreationCalendrierDepuisModeleController setup");
 		stagiaire = new Stagiaire();
 		stagiaire = stagiaireService.findBycodeStagiaire(20);
 		lieux = lieuService.findAllLieux();
@@ -115,6 +127,13 @@ public class CreationCalendrierVideController implements Serializable {
 	 */
 	public StagiaireServiceInterface getStagiaireService() {
 		return stagiaireService;
+	}
+
+	/**
+	 * @return the utilisateurService
+	 */
+	public UtilisateurServiceInterface getUtilisateurService() {
+		return utilisateurService;
 	}
 
 	/**
@@ -140,6 +159,14 @@ public class CreationCalendrierVideController implements Serializable {
 		this.lieuService = lieuService;
 	}
 
+	/**
+	 * @param utilisateurService
+	 *            the utilisateurService to set
+	 */
+	public void setUtilisateurService(UtilisateurServiceInterface utilisateurService) {
+		this.utilisateurService = utilisateurService;
+	}
+
 	public void onDateSelect(SelectEvent event) {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
@@ -149,6 +176,26 @@ public class CreationCalendrierVideController implements Serializable {
 	public void click() {
 		PrimeFaces.current().ajax().update("form:display");
 		PrimeFaces.current().executeScript("PF('dlg').show()");
+	}
+
+	/**
+	 * Permet de modifier un utilisateur
+	 *
+	 * @throws IOException
+	 */
+	public void creerModele(String typeAction) throws IOException {
+		HttpSession session = SessionUtils.getSession();
+		session.setAttribute(SessionUtils.SESSION_TYPE_ACTION, typeAction);
+		if (typeAction.equals("vide")) {
+			FacesContext.getCurrentInstance().getExternalContext()
+					.redirect("/eni-calendar/views/creationCalendrierModeleVide.xhtml");
+		} else if (typeAction.equals("depuisModele")){
+			FacesContext.getCurrentInstance().getExternalContext()
+					.redirect("/eni-calendar/views/creationCalendrierDepuisModele.xhtml");
+		} else {
+			FacesContext.getCurrentInstance().getExternalContext()
+					.redirect("/eni-calendar/views/creationCalendrierDepuisStagiaire.xhtml");
+		}
 	}
 
 	/**
