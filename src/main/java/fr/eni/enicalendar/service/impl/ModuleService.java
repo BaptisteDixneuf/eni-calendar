dc5abcd3d5ab5f7899f2b75cff13d7fb41c7cc8b
Dixneuf Baptiste <baptiste.dixneuf@gmail.com>
2018-08-24 01:21:15 +0200

    WIP

--- src/main/java/fr/eni/enicalendar/service/impl/ModuleService.java
+++ src/main/java/fr/eni/enicalendar/service/impl/ModuleService.java
@@ -2,6 +2,8 @@ package fr.eni.enicalendar.service.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,8 @@ import fr.eni.enicalendar.service.ModuleServiceInterface;
 
 @Service
 public class ModuleService implements ModuleServiceInterface {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ModuleService.class);
 
 	@Autowired
 	private ModuleRepository moduleRepository;
