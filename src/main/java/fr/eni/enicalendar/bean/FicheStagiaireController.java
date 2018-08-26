7f7b942a15b1551b0981f2af896611ce964fbfa3
axel <pironaxel@gmail.com>
2018-08-26 11:12:10 +0200

    gestion modification module

--- src/main/java/fr/eni/enicalendar/bean/FicheStagiaireController.java
+++ src/main/java/fr/eni/enicalendar/bean/FicheStagiaireController.java
@@ -15,18 +15,14 @@ import org.slf4j.LoggerFactory;
 import com.lowagie.text.BadElementException;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
-import com.lowagie.text.Image;
 import com.lowagie.text.PageSize;
-import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import org.primefaces.component.export.PDFOptions;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ViewScoped;
